dfa68b0faeeb0383d1858963406b05a972182553
hwupatrickcampbell <pat1cam@yahoo.co.uk>
2023-06-02 15:54:54 +0100

    Completed Lab 2

---
+++ front-end/src/pages/basketPage.vue
@@ -0,0 +1,24 @@
+<template>
+
+<div class="wrapper">
+    <div class="box a">Basket</div>
+	<div class="box b">Checkout</div>
+</div>
+
+<div class="center-bar">
+    <footer>
+        <p style="text-align:center">Copyright &copy; 2023 - F28IR</p>
+    </footer>
+</div>
+</template>
+  
+  <script>
+  export default {
+    name: 'basketPage',
+    components: {}
+  }
+  </script>
+  
+  <style scoped>
+
+  </style>
---
+++ front-end/src/pages/signinPage.vue
@@ -0,0 +1,39 @@
+<template>
+    
+<div id="login-box">
+    <div class="left">
+        <h1>Sign in</h1>
+        <form action="/signin" method="post">
+            <input type="text" v-model="userData.username" placeholder="Username" />
+            <input type="password" name="password" placeholder="Password" value="" />
+            <input type="submit" name="signup_submit" value="Sign me up" />
+        </form>
+    </div>
+  
+    <div class="right">
+       <span class="loginwith">Sign in with<br />social network</span>
+       <button class="social-signin facebook">Log in with facebook</button>
+       <button class="social-signin twitter">Log in with Twitter</button>
+       <button class="social-signin google">Log in with Google+</button>
+    </div>
+    <div class="or">OR</div>
+</div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'signinPage',
+    data() {
+        return {
+            userData: {
+                username: '',
+                password: '',
+            }
+        }
+    }
+  }
+  </script>
+  
+  <style scoped>
+
+  </style>
